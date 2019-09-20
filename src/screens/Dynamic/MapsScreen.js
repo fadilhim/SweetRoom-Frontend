9a65a06ac8d198e095465c702c881858e138ee9f
Poko Groot <PokoGroot@users.noreply.github.com>
2019-09-20 22:40:05 +0700

    Add map

--- src/screens/Dynamic/MapsScreen.js
+++ src/screens/Dynamic/MapsScreen.js
@@ -2,18 +2,18 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, } from 'react-native'
 import { View, } from 'native-base'
-import { ScrollView } from 'react-native-gesture-handler'
 import MapView, {PROVIDER_GOOGLE, Marker } from 'react-native-maps'
-import firebase from 'firebase'
-import Geolocation from 'react-native-geolocation-service'
-import Axios from 'axios'
-import AsyncStorage from '@react-native-community/async-storage'
+import MapViewDirections from 'react-native-maps-directions'
 
 class MapScreen extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            item: props.navigation.getParam('item'),
+            position: props.navigation.getParam('position'),
+            userPosition: {
+                latitude: -7.250445,
+                longitude: 112.768845,
+            }
         }
     }
 
@@ -22,14 +22,16 @@ class MapScreen extends Component {
     }
 
     render() {
+        console.log(this.state.position.lat)
+        console.log(this.state.position.long)
         return ( 
             <View style={styles.viewStyles}>
                 <MapView
                     style={{width: '100%', height: '100%'}}
                     provider={PROVIDER_GOOGLE}
                     region={{
-                        latitude: 0,
-                        longitude: 0,
+                        latitude: parseInt(this.state.position.lat) || 0,
+                        longitude: parseInt(this.state.position.long) || 0,
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
                     }}
@@ -37,15 +39,22 @@ class MapScreen extends Component {
                     showsUserLocation={true}
                     followUserLocation={true}
                 >
-                    {/* <Marker
-                        title={user.fullname || null}
-                        description={user.status || null}
-                        key={this.state.userPosition}
+                    <Marker
+                        // title={user.fullname || null}
+                        // description={user.status || null}
+                        key={this.state.position.long + this.state.position.lat}
                         coordinate={{ 
-                            latitude: this.state.userPosition.latitude,
-                            longitude: this.state.userPosition.longitude
+                            latitude: parseInt(this.state.position.lat) || 0,
+                            longitude: parseInt(this.state.position.long) || 0
                         }}
-                    > </Marker> */}
+                    />
+                    <MapViewDirections
+                        origin={this.state.userPosition}
+                        destination={{ latitude: parseInt(this.state.position.lat), longitude: parseInt(this.state.position.long) }}
+                        apikey={'AIzaSyDqf0uTouHVU39Q3ZSYPJivEVC2E6oqTH4'}
+                        // strokeWidth={3}
+                        // strokeColor="hotpink"
+                    />
                 </MapView>
             </View>
         )}
